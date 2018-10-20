db7846c3914f8d2b158b15923bf3ae807dfee4a2
Zipexpo <nvtngan@hcmus.edu.vn>
2018-10-20 16:03:24 -0700

    remove

--- voyager2/bower_components/compassql/src/query/transform.ts
+++
@@ -1,8 +0,0 @@
-import {Filter} from 'vega-lite/src/filter';
-import {Formula} from 'vega-lite/src/transform';
-
-export interface TransformQuery {
-  calculate?: Formula[];
-  filter?: Filter | string | (Filter|string)[];
-  filterInvalid?: boolean;
-}
--- voyager2/bower_components/vega-lite-ui/src/filters/compactjson/compactjson.filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('vlui')
-  .filter('compactJSON', function(JSON3) {
-    return function(input) {
-      return JSON3.stringify(input, null, '  ', 80);
-    };
-  });
