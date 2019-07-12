8865039acd6ff3fed390c8ee2d29d699c76f874a
Zipexpo <nvtngan@hcmus.edu.vn>
2019-07-12 17:05:19 -0500

    push

--- VAST19/mc1/src/scripts/tooltip.js
+++ VAST19/mc1/src/scripts/tooltip.js
@@ -25,13 +25,10 @@ function tooltipBox (data){
     let id = isNaN(+data.loc)?+data.loc.replace('s',''):+data.loc;
     let headertext = '<b>'+(type==="Summary"? type: dataRaw.location[data.loc])+'</b>';
     // headertext+=' - '+ data.time.toUTCString();
-    let headerdiv = d3.select('.tootltip_text').append('div').attr('class','header row s12');
-    headerdiv.append('p').attr('class','col').html(headertext);
-    headerdiv.append('h6').attr('class','col reportNum').text(data.data.num+' report'+ (data.data.num>1?'s':''));
-    d3.select('.tootltip_text').append('h6').text(data.time.toUTCString()).styles({
-        'margin-top':0,
-        'margin-left': '5px'
-    });
+    let headerdiv = d3.select('.tootltip_text').append('div').attr('class','header col s12');
+    headerdiv.append('h6').attr('class','col').html(headertext);
+    headerdiv.append('h6').attr('class','col').html('Time: <b>'+d3.timeFormat("%I %p - %a %d")(data.time)+'</b>');
+    headerdiv.append('h6').attr('class','col').html('#reports: <b>'+data.data.num+'</b>');
     // let table = d3.select('.tootltip_text').append('div').attr('class','tip-divtable')
     //     .append('table').attr('class','tip-table');
     // // table.append('thead').selectAll('th')
@@ -43,9 +40,12 @@ function tooltipBox (data){
     //     {key:'q1',text:'Q1'},
     //     {key:'q3',text:'Q2'}
     // ];
-    //
+    // let datatable = [['Time',d3.timeFormat("%I %p - %a %d")(data.time)],
+    //     ['#reports',data.data.num]
+    //     ]
+    // //
     // let tr = table.append('tbody').selectAll('tr')
-    //     .data(mm.map(k=>[k.text,data.data[k.key].toFixed(2)]))
+    //     .data(datatable)
     //     .enter().append('tr')
     //     .selectAll('td').data(d=>d)
     //     .enter().append('td').text(d=>d);
