5656c29b5d1d71fdf3ed6a02945c68392b093a93
Zipexpo <nvtngan@hcmus.edu.vn>
2019-07-20 13:35:19 -0500

    up max limit

--- VAST19/mc3/src/scripts/main.js
+++ VAST19/mc3/src/scripts/main.js
@@ -645,7 +645,7 @@ function onmouseoverRadar ([d,list]) {
     d.messagearr.forEach(e=>e.htmlLocation = markWord(e.location,list.filter(f=>f.group==='location_post')));
     let nestmap = d3.nest().key(e=>e.location).rollup(e=>e.length).entries(d.messagearr).filter(e=>e.key!=="<Location with-held due to contract>");
     d3.selectAll('.geoPath:not(#'+_.unique(d.messagearr.map(e=>removeWhitespace(e.location))).join('):not(#')+')').classed('nothover',true).style('fill',colorLegend(0)).each(d=>d.density = 0);
-    const step_val  = Math.max(10,d3.max(nestmap,e=>e.value))/colorLegend_array.length;
+    const step_val  = Math.max(30,d3.max(nestmap,e=>e.value))/colorLegend_array.length;
     colorLegend.domain(d3.range(0,colorLegend_array.length).map((d,i)=>i*step_val));
     colormap(colorLegend);
     nestmap.forEach(e=> d3.selectAll('.geoPath#'+removeWhitespace(e.key)).style('fill',colorLegend(e.value)).each(f=>f.density = e.value));
