8fb5bada3c9f2815a3d2360c35c89db7b3cd4f81
Zipexpo <nvtngan@hcmus.edu.vn>
2019-07-20 18:20:40 -0500

    update

--- VAST19/mc3/src/scripts/TimeArc.js
+++ VAST19/mc3/src/scripts/TimeArc.js
@@ -658,7 +658,7 @@ d3.TimeArc = function () {
             .style("stroke", function (d) {
                 return d.isSearchTerm ? "#000" : "#000";
             })
-            .style("stroke-width", 0.05)
+            .style("stroke-width", 1)
             .style("stroke-opacity", 0.5)
             .style("fill-opacity", 1)
             .style("fill", function (d, i) {
