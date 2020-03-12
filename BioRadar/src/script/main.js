249dde739e6cc8f4c136528aacbd5d63e5fdcb15
Zipexpo <nvtngan@hcmus.edu.vn>
2020-03-11 19:05:39 -0500

    init

--- BioRadar/src/script/main.js
+++ BioRadar/src/script/main.js
@@ -412,7 +412,71 @@ function main() {
     // MetricController.data(data.result.arr).drawSummary(data.result.hindex);
     init = false;
     firstTime = false;
+
+    network()
+
     // request();
+}
+function network(){
+    let dataTemp = handle_data_model(tsnedata);
+    let dataNest = d3.nest().key(d=>d.name).rollup(d=>(temp = d[1].map((e,i)=>e-d[0][i]>0?1:(e-d[0][i]<0?-1:0)),temp.cluster=d[0].cluster===d[1].cluster?d[0].cluster:-1,temp)).object(dataTemp);
+    let data ={
+        links:[],
+        nodes:_.keys(dataNest).map(d=>({id:d,group:1}))
+    }
+
+    for (let i= 0; i< data.nodes.length;i++){
+        for (let j= i+1; j< data.nodes.length;j++) {
+            let dis = distance(dataNest[data.nodes[i].id],dataNest[data.nodes[j].id])<2;
+            if(dis<2){
+                data.links.push({source: data.nodes[i].id, target: data.nodes[j].id, value: 1/dis})
+            }
+        }
+    }
+    console.log(JSON.stringify(data));
+    const links = data.links.map(d => Object.create(d));
+    const nodes = data.nodes.map(d => Object.create(d));
+
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    const svg = d3.select("svg#network")
+        .attr("viewBox", [0, 0, width, height]);
+
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
+
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 5)
+        .attr("fill", color)
+        .call(drag(simulation));
+
+    node.append("title")
+        .text(d => d.id);
+
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+    });
 }
 var currentlastIndex;
 var speedup= 0;
