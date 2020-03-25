5c494e4f95c46919feae94968ab43c47cc1391a9
nvtngan <nvtngan@hcmus.edu.vn>
2020-03-25 02:48:11 -0500

    update new data

--- COVID-19/src/scripts/generate.js
+++ COVID-19/src/scripts/generate.js
@@ -1,3 +1,6 @@
-csv = d3.keys(d[0]).join(',')
-csv+= '\n'+d.map(e=>d3.values(e).map(f=>`"${f}"`)).join('\n')
-console.log(csv)
+d3.csv('src/data/raw/covid19_processed_short.csv').then(d=>{
+    d=d.filter(e=>e.term!==""&&e.publish_time!==""&&!_.isNaN(+new Date(e.publish_time))&&filterYear(e));
+    csv = d3.keys(d[0]).join(',');
+    csv+= '\n'+d.map(e=>d3.values(e).map(f=>`"${f.replace(/"/gi,'')}"`).join(',')).join('\n');
+    console.log(csv)
+})
