67b03de7cfa7c8dfcec5cc7c9d2a2be9e6499f74
nvtngan <nvtngan@hcmus.edu.vn>
2020-03-26 23:17:27 -0500

    Update covid19data.js

--- wordStream/src/script/covid19data.js
+++ wordStream/src/script/covid19data.js
@@ -962,7 +962,7 @@ function fillData(endDate, startDate) {
     TermwDay = d;
 }
 let inclideList = ["WHO", "H1N1", "SEIR", "Zika", "FDA", "CDC", "COPD", "Iran"];
-let nonsenseCharacter = new RegExp(/\)|\(|&|\/|~|=|≥|[0-9]-[0-9]/gi);
+let nonsenseCharacter = new RegExp(/\)|\(|&|\/|~|=|≥|[0-9]-[0-9]|α|</gi);
 function blacklist(data){
     var numterm =0;
     categories = Object.keys(categoriesgroup);
@@ -974,7 +974,7 @@ function blacklist(data){
     data.forEach(d=>{
         d.keywords.filter(w => {
             numterm++;
-            var key = inclideList.find(d=>w.term===d);
+            var key = !!inclideList.find(d=>w.term===d);
             //categories.forEach(c=> key = key || ((w.category==c)&&(blackw.find(d=>d==w.term)== undefined)));
             key = key || w.term.length>4 && ((blackw.find(d=>d===w.term)== undefined)) && categoriesmap[w.category]!= undefined && !nonsenseCharacter.test(w.term) ;
             return key;}).forEach( w => {
